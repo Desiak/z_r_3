a15bba1e766776aa6436c99b9e32769c09af1efa
Desiak <desqa16@gmail.com>
2021-12-05 12:13:07 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [data, setData] = useState(null);
+  const [prevIndex,setPrevIndex]=useState(null);
+  const [quoteIndex, setQuoteIndex]= useState(null);
+
+  const getNewQuote=()=>{
+    setPrevIndex(quoteIndex);
+    const quoteInd= Math.floor(Math.random()*data.length);
+    setQuoteIndex(quoteInd)
+  }
+
+  const getPrevQuote=()=>{
+    setQuoteIndex(prevIndex);
+  }
+
+  useEffect(() => {
+    fetch("https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json")
+    .then((response) => {
+    if (response.ok) {
+      setData(response)
+    return response.json();
+    }
+    
+    }).then(json=>{
+      setData(json)
+    })
+    .catch((error) => {
+    console.error("Error fetching data: ", error);
+    })
+  
+    
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Zadanie 3</h1>
+     <button className="btn" onClick={()=>{getPrevQuote()}} disabled={quoteIndex===prevIndex}>Poprzedni cytat</button>
+     <button className="btn" onClick={()=>{getNewQuote()}}>Losuj</button>
+     <p>{data&&data[quoteIndex] && data[quoteIndex].quote}</p>
+     <p>{data&&data[quoteIndex] && data[quoteIndex].author}</p>
     </div>
   );
 }
